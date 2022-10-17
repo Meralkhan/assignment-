91c7439d891f6fd7bc8d789e2b108aa485d4c58a
Meralkhan <115946680+Meralkhan@users.noreply.github.com>
2022-10-17 09:33:16 +0500

    Add files via upload
    Assignment

---
+++ triangle(meral).java
@@ -0,0 +1,31 @@
+class calculateTriangle
+{
+    public static void main(String []args)
+    {
+        Triangle t = new Triangle(50, 10, 5, 10);
+        System.out.println("Area = " + t.Area());
+        System.out.println("Perimeter = " +
+t.Perimeter());
+} }
+class Triangle
+{
+    int height;
+    int base;
+    int m_a;
+    int m_c;
+    Triangle(int q, int r,int s,int u)
+    {
+        this.height = q;
+        this.base = r;
+        this.m_a = s;
+        this.m_c = u;
+}
+    int Area()
+    {
+        return (height * base)/2;
+    }
+    int Perimeter()
+    {
+        return m_a + base + m_c;
+    }
+}
